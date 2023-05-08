28c212ab57c18c4afe94fb1984dce3d6065d569a
patrick-ogb <ogbadapatrick@gmail.com>
2023-05-08 21:33:00 +0100

    added Paystack transfer model

---
+++ ReservationSyste/Models/PaystackTransferPayload.cs
@@ -0,0 +1,22 @@
+﻿namespace ReservationSyste.Models
+{ 
+    public class PaystackTransferPayload
+    {
+        public string currency { get; set; }
+        public string source { get; set; }
+        public Transfer[] transfers { get; set; }
+    }
+
+    public class Transfer
+    {
+        public int amount { get; set; }
+        public string reference { get; set; }
+        public string reason { get; set; }
+        public string recipient { get; set; }
+    }
+
+
+
+
+
+}
